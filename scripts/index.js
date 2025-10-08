686e2a645144bd2adc6fe1885beb686781d40c46
Enoti4Ka <Enoti4Ka44@gmail.com>
2025-10-08 13:05:24 +0300

    feat: add conditions to win

--- scripts/index.js
+++ scripts/index.js
@@ -58,11 +58,18 @@ const handleClearBoard = () => {
 
 //Check if player win
 const checkWin = () => {
-    //horizontal
+        //horizontal
     if (board[0][0] === board[0][1] && board[0][1] === board[0][2] && board[0][2] !== "" ||
         board[1][0] === board[1][1] && board[1][1] === board[1][2] && board[1][2] !== "" ||
-        board[2][0] === board[2][1] && board[2][1] === board[2][2] && board[2][2] !== ""
-    ) {
+        board[2][0] === board[2][1] && board[2][1] === board[2][2] && board[2][2] !== "" ||
+        //vartical
+        board[0][0] === board[1][0] && board[1][0] === board[2][0] && board[1][0] !== "" ||
+        board[0][1] === board[1][1] && board[1][1] === board[2][1] && board[1][1] !== "" ||
+        board[0][2] === board[1][2] && board[1][2] === board[2][2] && board[1][2] !== "" ||
+        //diagonal
+        board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[1][1] !== "" ||
+        board[0][2] === board[1][1] && board[1][1] === board[2][0] && board[1][1] !== "" )
+    {
         console.log('win')
         boardNode.classList.add('disabled')
         btnClear.classList.add('animation-pulse')
